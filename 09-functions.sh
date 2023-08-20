f7aa0e0d99e9406034dfbbc52421dd521598b277
chanti478 <info@joindevops.com>
2023-08-20 13:17:14 +0530

    hello world script added

--- 09-functions.sh
+++ 09-functions.sh
@@ -11,10 +11,10 @@ VALIDATE(){
 
     if [ $1 -ne 0 ]
 then
-    echo "$2 Installation  ...... failure"
+    echo "$2   ...... failure"
     exit 1
 else    
-    echo "$2 Installation  ........ success"
+    echo "$2   ........ success"
 
 fi
 }
