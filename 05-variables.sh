885802de6434b3dae8cb091a8c670923f5fbe716
chanti478 <info@joindevops.com>
2023-08-20 11:29:37 +0530

    hello world script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,9 @@ echo "please enter your name"
 read USERNAME # this is the variable where the value of the user and password
 
 echo "Username entered is: $USERNAME"
+
+echo "please enter your password"
+
+read PASSWORD
+
+echo "Password entered is: $PASSWORD"
