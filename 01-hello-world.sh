65995a501ae7532a95ee9e52cdf7734222076743
chanti478 <info@joindevops.com>
2023-08-20 09:02:42 +0530

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "hello world i am learning devops"
+
+echo "ramesh: good afternoon"
+echo "karthik: good afternoon ramesh"
+echo "ramesh: how are you karthik"
+echo "karthik: i am good ramesh"
