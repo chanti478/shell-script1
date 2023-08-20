6f6a18ed7a11d82122eba4f05b40123d26f9f9f0
chanti478 <info@joindevops.com>
2023-08-20 14:52:00 +0530

    hello world script added

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+R="\e[31m"
+N="\e[0m"
+
+if [ $USERID -ne 0];
+then
+    echo -e "$R ERROR:: please run this script with root access $N"
+    exit 1
+fi
 # all arguments are in $@
 for i in $@
 do
     yum install $i -y
 done
+
+# improvements
+    #implement log files
+    #implement clours
+    #implement validations
+    #implement validations through functions
+    #your script should check package is installed or not, if already installed print yellow colour
+    #If installed just print package is already installed, it should not  run install command
