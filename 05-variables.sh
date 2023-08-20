fcddee1a5468de6f915f99dca03c4f9a0a961cae
chanti478 <info@joindevops.com>
2023-08-20 11:44:23 +0530

    hello world script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,6 +3,7 @@
 echo "please enter your name"
 
 read -s USERNAME # this is the variable where the value of the user and password
+#this is only for valiadation, we should not print in the termnal
 
 echo "Username entered is: $USERNAME"
 
---
+++ 06-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this is called array, instead of single value it can hold multiple values
+
+PERSONS=("Ramesh" "suresh""raghu")
+
+echo "First Person: $(PERSONS[0])"
