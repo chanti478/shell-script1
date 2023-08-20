f28771065bc8d11b747071b5a50a19b32dc45336
chanti478 <info@joindevops.com>
2023-08-20 16:22:45 +0530

    hello world script added

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript1-logs
+# /home/centos/shellscript1-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 
 R="\e[31m"
 N="\e[0m"
+Y="\e[33m"
 
 if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR:: please run this script with root access $N"
     exit 1
 fi
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e " Installing $2 ... $R failure $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G success $N"
+    fi
+}
 # all arguments are in $@
 for i in $@
 do
-    yum install $i -y
+    yum list installed $i
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y
+        VALIDATE $? ""
+    else
+    echo -e "$Y $i is already installed $N"
+    fi
+    # yum install $i -y
 done
 
 # improvements
