90ed2e6db72168b053e8af4842d50a2f59761e9b
chanti478 <info@joindevops.com>
2023-08-22 15:25:44 +0530

    hello world script added

--- disk_usage.sh
+++ disk_usage.sh
@@ -17,10 +17,24 @@ Y="\e[33m"
 
 DISK_USAGE=$( df -hT | grep -vE 'tmpfs|filesystem')
 DISK_USAGE_THRESHOLD=1
+message=""
 
 # IFS- means internal faild seperator is space
 while IFS= read line
 do
-    echo "output: $line"
+    # this command will give you usage in number format for comparision
+    USAGE=$(echo$line | wk '{print $6}' | cut -d % -f1)
+    # This command will give us partition
+    partition=$(echo$line  | awk '{print $1}')
+    #now you need to check whether it is more than threshold or not
+    if [$usage -gt $DISK_USAGE_THRESHOLD];
+    then
+        message+="HIGH DISK USAGE ON $partition: $usage"
+        fi
+
+
 done <<< $DISK_USAGE
+
+echo "message: $message"
+
 
